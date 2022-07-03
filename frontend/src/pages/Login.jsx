2440b469f5bc9502b06d5de9965bad29488fa29d
Hagai Raja Sinulingga <hagairaja@gmail.com>
2022-07-04 00:57:16 +0900

    add frontend using react and redux

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,5 +1,10 @@
-import {useState, userEffect} from 'react'
-import {FaSignInAlt} from 'react-icons/fa'
+import { useState, useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import { toast } from 'react-toastify'
+import { FaSignInAlt } from 'react-icons/fa'
+import { login, reset } from '../features/auth/authSlice'
+import Spinner from '../components/Spinner'
 
 function Login() {
   const [formData, setFormData] = useState({
@@ -9,6 +14,13 @@ function Login() {
 
   const {email, password} = formData
 
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+
+  const { user, isLoading, isError, isSuccess, message } = useSelector(
+    (state) => state.auth
+  ) 
+
   const onChange = (e) => {
     setFormData((prevState) => ({
       ...prevState,
@@ -16,8 +28,31 @@ function Login() {
     }))
   }
 
+  useEffect(() => {
+    if (isError) {
+      toast.error(message)
+    }
+    
+    if (isSuccess || user) {
+      navigate('/')
+    }
+
+    dispatch(reset())
+  }, [user, isError, isSuccess, message, navigate, dispatch])
+
   const onSubmit = (e) => {
     e.preventDefault()
+
+    const userData = {
+      email,
+      password,
+    }
+
+    dispatch(login(userData))
+  }
+
+  if (isLoading) {
+    return <Spinner />
   }
 
   return (
