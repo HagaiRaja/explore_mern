f6ca1c2797f05ac671dee65f8783974c19d9532f
Hagai Raja Sinulingga <hagairaja@gmail.com>
2022-07-12 17:45:46 +0900

    Finish Goal UI

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -41,14 +41,13 @@ const updateGoal = asyncHandler( async (req, res) => {
   }
 
   // Check for user
-  const user = await User.findById(req.user.id);
-  if (!user) {
+  if (!req.user) {
     res.status(401);
     throw new Error('User not found');
   }
 
   // Make sure the logged in user matches the goal user
-  if (goal.user.toString() !== user.id) {
+  if (goal.user.toString() !== req.user.id) {
     res.status(401);
     throw new Error('User not authorized');
   }
@@ -74,14 +73,13 @@ const deleteGoal = asyncHandler( async (req, res) => {
   }
 
   // Check for user
-  const user = await User.findById(req.user.id);
-  if (!user) {
+  if (!req.user) {
     res.status(401);
     throw new Error('User not found');
   }
 
   // Make sure the logged in user matches the goal user
-  if (goal.user.toString() !== user.id) {
+  if (goal.user.toString() !== req.user.id) {
     res.status(401);
     throw new Error('User not authorized');
   }
